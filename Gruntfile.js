9a7745a429cb9ca28ace74de1f47edf234bb12cb
Tyler Mauthe <me@tylermauthe.com>
2013-11-03 01:24:07 -0600

    Add CI and Automated Tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,47 @@
 module.exports = function (grunt) {
+    var browsers = [{
+        browserName: "internet explorer",
+        version: "8",
+        platform: "WIN7"
+    },
+    {
+        browserName: "internet explorer",
+        version: "9",
+        platform: "WIN7"
+    }];
     grunt.initConfig({
 
-    clean: ["bin/"],
-    // define source files and their destinations
     uglify: {
-        files: { 
-            src: 'src/jquery.sexy-placeholder.js',  // source files mask
-            dest: 'bin/',    // destination folder
-            expand: true,    // allow dynamic building
-            flatten: true,   // remove all unnecessary nesting
-            ext: '.sexy-placeholder.min.js'   // replace .js to .min.js
+        files: {
+            src: 'src/jquery.sexy-placeholder.js',
+            dest: 'bin/',
+            expand: true,
+            flatten: true,
+            ext: '.sexy-placeholder.min.js'
         }
     },
-    watch: {
-        js:  { files: 'js/*.js', tasks: [ 'uglify' ] },
+    'saucelabs-jasmine': {
+        all: {
+            options: {
+                urls: ["http://127.0.0.1:9999/test/SpecRunner.html"],
+                tunnelTimeout: 5,
+                build: process.env.TRAVIS_JOB_ID,
+                concurrency: 2,
+                browsers: browsers,
+                testname: "jQuery Sexy Placeholder",
+                tags: ["master"]
+            }
+        }
     },
+    connect: {
+      server: {
+        options: {
+          base: "",
+          port: 9999
+        }
+      }
+    },
+    watch: {},
     copy: {
       main: {
         files: [
@@ -24,17 +51,21 @@ module.exports = function (grunt) {
     },
     jshint: {
         all: ['Gruntfile.js', 'spec/*.js', 'src/*.js']
-    }
+    },
+    clean: ["bin/"]
 });
 
 // load plugins
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-saucelabs');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-clean');
 
 // register at least this one task
 grunt.registerTask('default', [ 'clean', 'jshint', 'uglify', 'copy' ]);
-
+grunt.registerTask('dev', ['connect', 'watch']);
+grunt.registerTask("test", ["connect", "saucelabs-jasmine"]);
 };
